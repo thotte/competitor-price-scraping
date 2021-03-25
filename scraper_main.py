a60c9c0c0c34577f541c32def737e857861385fa
Thomas Österling <thomas.osterling@consid.se>
2021-03-25 19:36:22 +0100

    Add gitlab-ci.yml

--- scraper_main.py
+++ scraper_main.py
@@ -5,11 +5,9 @@
 
 import logging
 from comviq import Comviq
-from fello import Fello
 from hallon import Hallon
 from halebop import Halebop
 from tele2 import Tele2
-from telia import Telia
 from tre import Tre
 import pandas as pd
 
@@ -20,22 +18,20 @@ competitors = []
 pricelist = []
 
 # Create Competitors
-#competitors.append(Comviq("Comviq"))
-competitors.append(Fello("Fello"))
-#competitors.append(Hallon("Hallon"))
-#competitors.append(Halebop("Halebop"))
-#competitors.append(Tele2("Tele2"))
-#competitors.append(Telia("Telia"))
-#competitors.append(Tre("Tre"))
+competitors.append(Comviq("Comviq"))
+competitors.append(Hallon("Hallon"))
+competitors.append(Halebop("Halebop"))
+competitors.append(Tele2("Tele2"))
+competitors.append(Tre("Tre"))
 
 # Create priceplans for each competitor and append to a common dataframe
 for competitor in competitors:
-    #try:
+    try:
         priceplan = competitor.get_dataframe()
         prices = competitor.process_dataframe(priceplan)
         pricelist.append(prices)
-    #except:
-        #logging.error("Error getting prices for for %s", competitor.name)    
+    except:
+        logging.error("Error getting prices for for %s", competitor.name)    
 
 # Format pricelist into dataframe
 Large_df = pd.concat(pricelist, ignore_index=True)
